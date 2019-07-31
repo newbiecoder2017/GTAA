0d21a48f394bb05a91f31df2811e08146cc0369b
newbiecoder2017 <fallinlove_l@yahoo.com>
2019-07-31 19:09:54 -0400

    Rebalance 7/31/2019

--- SectorsV2/SectorRotation_v2.py
+++ SectorsV2/SectorRotation_v2.py
@@ -39,7 +39,7 @@ today = datetime.datetime.today().strftime('%m/%d/%y')
 #Function to pull data from yahoo
 def pull_data(s):
 
-    return pdr.get_data_yahoo(s, start="2000-12-31", end="2019-06-28")['Adj Close']
+    return pdr.get_data_yahoo(s, start="2000-12-31", end="2019-07-31")['Adj Close']
 
 def read_price_file(frq = 'BM'):
     df_price = pd.read_csv("C:/Python27/Git/SMA_GTAA/Sectors/adj_close_sectors.csv", index_col='Date', parse_dates=True)
